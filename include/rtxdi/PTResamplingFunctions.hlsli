60d0262baeef21e2450a9ec19aea0438084b5aba
Thomaswang0822 <thomaswang0822@outlook.com>
2024-07-21 21:44:09 -0700

    init files for ReSTIR PT

---
+++ include/rtxdi/PTResamplingFunctions.hlsli
@@ -0,0 +1,16 @@
+/***************************************************************************
+ # Copyright (c) 2023, NVIDIA CORPORATION.  All rights reserved.
+ #
+ # NVIDIA CORPORATION and its licensors retain all intellectual property
+ # and proprietary rights in and to this software, related documentation
+ # and any modifications thereto.  Any use, reproduction, disclosure or
+ # distribution of this software and related documentation without an express
+ # license agreement from NVIDIA CORPORATION is strictly prohibited.
+ **************************************************************************/
+
+#ifndef PT_RESAMPLING_FUNCTIONS_HLSLI
+#define PT_RESAMPLING_FUNCTIONS_HLSLI
+
+#include "PTReservoir.hlsli"
+
+#endif // PT_RESAMPLING_FUNCTIONS_HLSLI
---
+++ include/rtxdi/ReSTIRPTParameters.h
@@ -0,0 +1,17 @@
+/***************************************************************************
+ # Copyright (c) 2020-2023, NVIDIA CORPORATION.  All rights reserved.
+ #
+ # NVIDIA CORPORATION and its licensors retain all intellectual property
+ # and proprietary rights in and to this software, related documentation
+ # and any modifications thereto.  Any use, reproduction, disclosure or
+ # distribution of this software and related documentation without an express
+ # license agreement from NVIDIA CORPORATION is strictly prohibited.
+ **************************************************************************/
+
+#ifndef RTXDI_RESTIRPT_PARAMETERS_H
+#define RTXDI_RESTIRPT_PARAMETERS_H
+
+#include "RtxdiTypes.h"
+#include "RtxdiParameters.h"
+
+#endif // RTXDI_RESTIRPT_PARAMETERS_H
